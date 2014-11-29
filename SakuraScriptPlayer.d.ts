8ec4b99ce8ea6b1f276c847def616e8c468380c8
Legokichi Duckscallion <legokichi@duxca.com>
2014-11-30 03:42:55 +0900

    fix

--- SakuraScriptPlayer.d.ts
+++ SakuraScriptPlayer.d.ts
@@ -1,24 +1,13 @@
 
 declare class SakuraScriptPlayer {
-  constructor(named: Named); // stable
-  play: (sakuraScript: string, listener?: {[event_name: string]: () => void;}): void; // unstable
-  break: (): void; // stable
-  on: (event: string, callback: () => void;): SakuraScriptPlayer;
-  off: (event?: string, callback?: () => void;): SakuraScriptPlayer;
-  trigger: (event: string, ...args?: any[]): SakuraScriptPlayer;
-  trigger_local: (event: string, listener?: {[event_name: string]: () => void;}, ...args?: any[]): SakuraScriptPlayer;
-  trigger_all: (event: string, listener?: {[event_name: string]: () => void;}, ...args?: any[]): SakuraScriptPlayer;
-  breakTid: number; // unstable
-  playing: boolean; // unstable
-}
-
-declare module Nar {
-}
-
-declare module 'nar' {
-  var foo: typeof Nar;
-  module rsvp {
-    export var Nar: typeof foo;
-  }
-  export = rsvp;
+  constructor(named: Named);
+  play(sakuraScript: string, listener?: {[event_name: string]: () => void;}): void;
+  break(): void;
+  on( event:  string, callback:  () => void): SakuraScriptPlayer;
+  off(event?: string, callback?: () => void): SakuraScriptPlayer;
+  trigger(      event: string, ...args: any[]): SakuraScriptPlayer;
+  trigger_local(event: string, listener?: {[event_name: string]: () => void;}, ...args: any[]): SakuraScriptPlayer;
+  trigger_all(  event: string, listener?: {[event_name: string]: () => void;}, ...args: any[]): SakuraScriptPlayer;
+  breakTid: number;
+  playing: boolean;
 }
